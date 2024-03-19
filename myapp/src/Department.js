5f553e6478946d0f3a17cc618e67b8f47160952d
Harsh <harshchoprajpr@gmail.com>
2024-03-20 00:41:11 +0530

    login page api added

--- myapp/src/Department.js
+++ myapp/src/Department.js
@@ -1,14 +1,15 @@
 import React from "react"
-import Navbar from "./Components/Navbar"
+
 import DeptCard from "./Components/DeptCard"
 // import DeptDetail from "./DeptDetail"
 
+import Navbar from "./Components/component/Nav"
 
 
 export default function Department(){
   return(
     <div>
-        <Navbar />
+        <Navbar/>
             <div className="MainContent">
             <DeptCard />
             {/* <DeptDetail/>  */}
--- myapp/src/Home.js
+++ myapp/src/Home.js
@@ -1,142 +1,89 @@
-import { useEffect, useState } from "react";
-import img1 from "../src/Components/images/main title.png";
-import img2 from "../src/Components/images/bg login illustration.png";
-import img3 from "../src/Components/images/bglogin.png";
+import React, { useState } from "react";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
 import Homepage from "./Homepage";
-import { Hidden } from "@mui/material";
+import img2 from "../src/Components/images/main title.png";
+import img1 from "../src/Components/images/bg login illustration.png";
+import img3 from "../src/Components/images/bglogin.png";
+import { params } from "react-router-dom";
 
-function Home() {
-  const [ldapid, setldapid] = useState();
-  const [password, setpassword] = useState();
+export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [data, setdata] = useState();
   const navigate = useNavigate();
 
-  const handlename = (event) =>{
-    setldapid(event.target.value);
-  }
-  const handlepassword =(event) =>{
-    setpassword(event.target.value);
-  }
-  const handlehomepage =(()=>{
-    navigate("/homepage");
-  })
-    
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
 
- /* const handlelogin =(e)=>{
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = (e) => {
     e.preventDefault();
-    axios.post(" " , {
-      'ldapid':ldapid,
-      password:password
-    }) 
-    .then((response=>{
-      console.log(response.data)
-      alert("success")
-      //there comes login into home page
-      localStorage.setItem('token',response.data.token)
-      //saving token on local storage
-      navigate('/')  ; // link of page1 by aman
-      setpassword("");
-      setldapid("");  }))
+    axios
+      .post("http://127.0.0.1:8000/users/login/", {
+        email,
+        password,
+      })
+      .then((res) => {
+        navigate("/homepage");
+      })
+      .catch((error) => {
+        console.log("Login failed:", error);
+      });
+    console.log(email);
+    console.log(password);
+    setEmail("");
+    setPassword("");
+  };
 
-    .catch((error=>{
-      console.log(" there is error " , error )
-    }))
-    
-  }
-    const handleLogin = (e) => {
-        e.preventDefault();
-        if (!ldap-is || !password ) {
-            alert("Please enter name, email, and password to login.");
-            return; // Prevent API call if fields are empty
-        }
-        axios.post(" ", {
-            'ldapid':ldapid,
-            password:password
-    }) 
-        
-       .then((response)=>{
-        console.log(response);
-        const users  = response.data;
-        if(users){
-          const user = users.find(use=>use.ldapid === ldapid && use.password === password );
-            if(user){
-              navigate('');
-            }
-            else{
-              alert("enter correct credentials ");
-            }
-          }
-          else{
-            alert("there is error logging in ")
-          }
-        }
-       )
-        .catch((error) => {
-            console.error("Error submitting data:", error);
-            alert("There was an error logging in");
-        });
-    }
-    */
   return (
-    <>
-    <div className="loginpage">
-      <div className="dfinal">
-        <div className="d1">
-          <img className="img1" src={img1}></img>
-        </div>
-        <div className="dnet">
-          <div className="d2">
-            <img className="img2" src={img2}></img>
+    <div className="full">
+      <img className="img2" src={img2}></img>
+
+      <div className="loginpage">
+        <img className="img1" src={img1}></img>
+        <form className="form" onSubmit={handleLogin}>
+          <label htmlFor="email">LDAP-ID</label>
+          <input
+            id="email"
+            type="text"
+            value={email}
+            onChange={handleEmail}
+            name="email"
+            placeholder="Enter LDAP-ID here "
+          />
+          <br />
+
+          <label htmlFor="password">Password</label>
+
+          <input
+            id="password"
+            name="password"
+            type="password"
+            value={password}
+            onChange={handlePassword}
+            placeholder="Enter password here"
+          />
+
+          <div id="checkbox">
+            <input type="checkbox" /> <p>remember me on the device </p> <br />
           </div>
-          <div className="d3">
-          <form className="form1" >
-              <label htmlFor="uername">LDAP-ID</label>
-              <br />
-              <input
-                id="ldapid"
-                type="text"
-                value={ldapid}
-                onChange={handlename}
-                name="ldapid"
-                placeholder="enter ldap-id here "
-              />
-              <br />
-              <br />
-              <label htmlFor="password">Password</label>
-              <br />
-              <input
-                id="password"
-                name="password"
-                type="password"
-                value={password}
-                onChange={handlepassword}
-                placeholder="enter password here"
-              />
-              <input type="checkbox" /> remember me on the device <br />
-              <br />
-              <button type="submit" className="btn btn-primary " style={{textDecoration:'none' , color:'white'}} onClick={handlehomepage}  >
-               Log in
-              </button>
-              <br />
-              <br />
-              <h14>
-               
-              <Link to="/home" style={{ color: "black", textDecoration: "none" }}>
-                  forget password ?
-                </Link>
-              </h14>
-              <br />
-            </form>
-          </div>
-        </div>
-        <div className="d4">
-          <img className="img3" src={img3}></img>
-        </div>
+          <br />
+          <button type="submit" className="btn btn-primary2">
+            Log in
+          </button>
+
+          <h4>
+            <Link to="/home" style={{ color: "black", textDecoration: "none" }}>
+              Forgot password?
+            </Link>
+          </h4>
+        </form>
       </div>
-    <div/>
     </div>
-    </>
   );
 }
-export default Home;
